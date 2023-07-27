60765e147b5dff9f1a209d8cd01922419f99b93c
auctumnus <auctumnus@pm.me>
2023-07-27 17:10:42 -0500

    fix: theme builder color scheme change

--- src/plugin.ts
+++ src/plugin.ts
@@ -59,12 +59,12 @@ export type ColorScheme = {
 
 export type Palette = {
     "0": string
-    "5": string,
+    "5"?: string,
     "10": string,
     "20": string,
-    "25": string,
+    "25"?: string,
     "30": string,
-    "35": string,
+    "35"?: string,
     "40": string,
     "50": string,
     "60": string,
